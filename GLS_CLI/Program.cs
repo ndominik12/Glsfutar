be72a88b47d060f03f5a7ca36b0b9c4e37f52e6a
Nagy Dominik <NagyD@KKSZKI.LOCAL>
2025-10-08 11:39:18 +0200

    Feladat 2 asd

--- GLS_CLI/Program.cs
+++ GLS_CLI/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GLS_CLI
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            string[] sorok = File.ReadAllLines("GLS.txt");
+            List<AutoAdatok> lista = new List<AutoAdatok>();
+
+            foreach (string sor in sorok)
+            {
+                if (!string.IsNullOrWhiteSpace(sor))
+                    lista.Add(new AutoAdatok(sor));
+            }
+
+            Console.WriteLine("2. Feladat:");
+            Console.WriteLine($"\tAz autó használatban töltött napjainak száma: {lista.Count}");
         }
     }
 }
